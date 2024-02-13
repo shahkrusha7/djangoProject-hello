beae703aacc5305c5c4ae60d1418e57a1c34f712
shahkrusha7 <krusha.shah2810@gmail.com>
2024-02-13 20:56:30 +0530

    basepage template has been addedand url codeblock has been added to navigation bar.

--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    #return HttpResponse("<h1>Hello</h1>")
     return render(request,"hello/home.html")
 
 def aboutus(request):
-    #return HttpResponse("<h1 style='color:blue'>About Us</h1>")
     return render(request,"hello/aboutus.html")
 
 def contactus(request):
-    #return HttpResponse("<h1>Contact Us</h1>")
     return render(request,"hello/contactus.html")
