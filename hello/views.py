4e5bcfa6999f76e5db17b1430c45bc1502db0088
shahkrusha7 <krusha.shah2810@gmail.com>
2024-02-13 20:01:29 +0530

    html pages has been render

--- hello/views.py
+++ hello/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hello</h1>")
+    #return HttpResponse("<h1>Hello</h1>")
+    return render(request,"hello/home.html")
 
 def aboutus(request):
-    return HttpResponse("<h1 style='color:blue'>About Us</h1>")
+    #return HttpResponse("<h1 style='color:blue'>About Us</h1>")
+    return render(request,"hello/aboutus.html")
 
 def contactus(request):
-    return HttpResponse("<h1>Contact Us</h1>")
+    #return HttpResponse("<h1>Contact Us</h1>")
+    return render(request,"hello/contactus.html")
