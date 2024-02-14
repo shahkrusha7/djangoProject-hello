33779ac5aca4b1060216ea74182620f78491d441
shahkrusha7 <krusha.shah2810@gmail.com>
2024-02-14 22:58:03 +0530

    product details displaying functionality implemented

--- newyear/urls.py
+++ newyear/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home,employeeview
+from .views import home,employeeview,productview
 
 urlpatterns = [
 path("",home,name="newyear-home"),
-    path("employee/",employeeview,name="employeeview")
+    path("employee/",employeeview,name="employeeview"),
+    path("product/",productview,name="productview")
 ]
